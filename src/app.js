825bfbfa3322252fe2ad5f7899694c4d0f36600f
Kanishk Gupta <kanishkgupta1234@gmail.com>
2021-04-15 23:30:09 +0530

    Initial setup

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import { hot } from 'react-hot-loader/root';
+import React, { useState } from 'react';
+
+
+function App() {
+
+    return (
+        <>
+            <div>
+                Test App
+            </div>
+        </>
+    );
+}
+
+export default hot(App);
